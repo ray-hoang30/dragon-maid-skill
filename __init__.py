2c7f3236b26277f984d4a47872a00f26a31face1
Raymond <raymond_1999@hotmail.com>
2021-09-22 19:41:49 -0400

    Adding preliminary stuff

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+import pafy
+import vlc
 
 
 class DragonMaid(MycroftSkill):
@@ -8,6 +10,16 @@ class DragonMaid(MycroftSkill):
     @intent_file_handler('maid.dragon.intent')
     def handle_maid_dragon(self, message):
         self.speak_dialog('maid.dragon')
+        url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
+		video = pafy.new(url)
+		best = video.getbest()
+		playurl = best.url
+		Instance = vlc.Instance()
+		player = Instance.media_player_new()
+		Media = Instance.media_new(playurl)
+		Media.get_mrl()
+		player.set_media(Media)
+		player.play()
 
 
 def create_skill():
