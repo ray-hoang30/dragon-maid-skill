138fdf730b2e820d54559c9c484c513c28495b4b
Raymond Hoang <raymond_1999@hotmail.com>
2022-02-04 01:30:27 -0500

    test

--- __init__.py
+++ __init__.py
@@ -9,17 +9,6 @@ class DragonMaid(MycroftSkill):
 
     @intent_file_handler('maid.dragon.intent')
     def handle_maid_dragon(self, message):
-        self.speak_dialog('maid.dragon')
-        url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
-		video = pafy.new(url)
-		best = video.getbest()
-		playurl = best.url
-		Instance = vlc.Instance()
-		player = Instance.media_player_new()
-		Media = Instance.media_new(playurl)
-		Media.get_mrl()
-		player.set_media(Media)
-		player.play()
 
 
 def create_skill():
